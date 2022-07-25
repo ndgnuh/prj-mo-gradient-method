6f01ffd65baea5ec074ccd8fcb3093ab6f8047c8
Hung Nguyen <hungnd@hiworld.com.vn>
2022-07-25 16:50:14 +0700

    thêm example 5

---
+++ examples/test_5.py
@@ -0,0 +1,17 @@
+import torch
+from src.vector_function import VectorFunction
+
+
+def f(x):
+    return (x[0]-1)**2 + 3 * (x[1] - 10)**2
+
+
+def g(x):
+    return 2 * (x[1] - 10)**4 + (x[0]-1)**2
+
+
+f = VectorFunction(f, g)
+max_epoch = 100
+lr = 0.5
+seed = 10
+n = 2
