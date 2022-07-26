3af7217baf14dc3f46043581a8346f50f85167fa
Hung Nguyen <hungnd@hiworld.com.vn>
2022-07-26 10:37:10 +0700

    chỉnh VD2

--- examples/test_2.py
+++ examples/test_2.py
@@ -8,9 +8,9 @@ def f1(x):
 
 def f2(x):
     n = x.shape[0]
-    g = 1 + 9 / (n - 1) * x[1:].norm()
+    g = 1 + 9 / (n - 1) * x[1:].sum()
     h = 1 - torch.sqrt(x[0] / g) - (x[0] * g) * torch.sin(10 * torch.pi * x[0])
-    return g * h
+    return torch.log((g * h) ** 2 + 2)
 
 
 f = VectorFunction(f1, f2)
